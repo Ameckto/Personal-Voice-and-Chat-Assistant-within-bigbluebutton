eaf201e7846bb4a9d1f04a1db5fba72f11e205da
Niklas <nvboguszewski@googlemail.com>
2020-08-18 09:25:58 +0200

    Update test.js

--- bigbluebutton-html5/public/Niklas/test.js
+++ bigbluebutton-html5/public/Niklas/test.js
@@ -1,3 +1,11 @@
 var my_test = function() {
+  import Users from '/imports/api/users';
+  import Auth from '/imports/ui/services/auth';
+
+  console.log(Auth)
+  console.log(Users)
+  const currentUser = Users.findOne({ userId: Auth.userID }, { fields: { role: 1, locked: 1 } });
+
+
   console.log('test');
 }
