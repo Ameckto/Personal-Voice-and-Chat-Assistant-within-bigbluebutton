8a7262978262bec2a94dd95cb64d1825a223e324
Niklas <nvboguszewski@googlemail.com>
2020-08-18 10:23:08 +0200

    Update index.js

--- bigbluebutton-html5/imports/api/voice-assistant/index.js
+++ bigbluebutton-html5/imports/api/voice-assistant/index.js
@@ -1,13 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 
-const VoiceCallStates = new Mongo.Collection('voiceCallStates');
+const VoiceCallStates = new Mongo.Collection('VoiceAssistant');
 
 if (Meteor.isServer) {
   // types of queries for the voice users:
   // 1. intId
   // 2. meetingId, intId
-
-  VoiceCallStates._ensureIndex({ meetingId: 1, userId: 1 });
+  //VoiceAssistant._ensureIndex({ meetingId: 1, userId: 1 });
 }
 console.log('This is my Voice VoiceAssistant Code B1tches');
+
+var in_test = function() {
+  console.log('in module');
+}
+
+
 export default VoiceAssistant;
