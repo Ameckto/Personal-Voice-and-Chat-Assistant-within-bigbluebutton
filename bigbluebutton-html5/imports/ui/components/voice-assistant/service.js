333a8da79961d71cb26384a6363225b34c8994b2
Niklas <nvboguszewski@googlemail.com>
2020-10-02 20:31:10 +0200

    Update service.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
@@ -5,6 +5,22 @@ import { Meteor } from 'meteor/meteor';
 
 console.log('in new voice assistaent')
 
+//gets PERSONS of intent and returns them in an array, can be multiple
+var get_person_of_intent = function(response, intent){
+  var result_arr = []
+  var entity_arr = response.entities
+  var arrayLength = entity_arr.length;
+
+  for (var i = 0; i < arrayLength; i++) {
+      var entity_type =  entity_arr[i].entity;
+      if (entity_type == 'PERSON') {
+        var person =  entity_arr[i].value;
+        result_arr.push(person)
+      }
+  }
+  return result_arr
+}
+
 // retrun intents in array >= min_confidence
 var filter_intent = function(intent_arr, min_confidence) {
   var result_arr = []
@@ -32,11 +48,57 @@ var check_intent = function(intent_arr, name) {
   return false
 }
 
+var mute_user = function(user) {
+  //get the _id, muted boolean and the name of the person to mute
+  const personToMute = () => {
+    const collection = VoiceUsers.findOne({ callerName: user});
+      console.log(collection);
+    return [collection._id, collection.muted, collection.callerName];
+  };
+
+  person = personToMute();
+
+  _id = person[0];
+  muted_boolean = person[1];
+  person_to_mute = person[2]
+
+  console.log('person_to_mute: ' + person_to_mute);
+
+  if (muted == false) {
+    //var user = VoiceUsers.findOne({callerName: person_to_mute});
+    VoiceUsers.update({_id: _id}, { $set: { 'muted': true }});
+  }else{
+    console.log('User is already muted')
+  }
+}
+
+var wake_up = function(client) {
+  sentence = 'Hey, what can I do for you ' + client + '?';
+  console.log(sentence);
+  const utterance = new SpeechSynthesisUtterance(sentence);
+  window.speechSynthesis.speak(utterance);
+}
+
+var execute_intent = function(intent, response) {
+
+  //get the name of the client person
+  client = VoiceUsers.findOne({ meetingId: Auth.meetingID, intId: Auth.userID }).callerName;
+
+  if (intent == 'mute') {
+      // the persons the client wants to mute
+      person_arr = get_person_of_intent(response, intent)
+      person_arr.forEach(mute_user(element))
+  }
+
+  if (intent == 'wake_up') {
+    wake_up(client)
+  }
+};
+
+
 class Voice_Assistant {
   constructor(item) {
     this._message = item.message;
-    //this._int_id = item.sender;
-    this._caller_name = VoiceUsers.findOne({ meetingId: Auth.meetingID, intId: Auth.userID }).callerName;
     console.log('_caller_name: ', this._caller_name)
     this._response = this.make_post_request(this._message)
   }
@@ -70,7 +132,8 @@ class Voice_Assistant {
                 var index = intent_arr.indexOf('wake_up')
                 intent_arr.splice(index, 1);
                 var intent = intent_arr[0]
-                console.log('2 inents: ', intent)
+                console.log('2 intent: ', intent)
+                execute_intent = function(intent, response)
               }
             } else {
 
@@ -114,7 +177,6 @@ var handle = GroupChatMsg.find().observe({
   added: function (item) {
     if (!initializing)
         // do stuff with newly added items, this check skips the first run
-        //
         console.log('something changed')
         console.log(item)
         a = new Voice_Assistant(item, min_confidence)
