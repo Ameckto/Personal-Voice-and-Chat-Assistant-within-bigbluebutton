9847610e1b4b4012d2dbb9e46b5caadcd3465b1b
Niklas <nvboguszewski@googlemail.com>
2020-10-04 18:54:01 +0200

    Update service.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
@@ -60,7 +60,7 @@ var mute_user = function(user) {
   var userId = get_userId(user)
   var is_user_muted = VoiceUsers.findOne({ callerName: user}).muted
   var users_role = Users.findOne({ name: user, connectionStatus: 'online'}).role;
-  console.log(is_user_muted)
+  console.log(users_role)
   //--------------
   const toggleVoice = (userId) => {
     if (userId === Auth.userID) {
