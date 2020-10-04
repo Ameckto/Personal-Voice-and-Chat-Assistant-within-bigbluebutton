f8b8b3351fc51d80ea4370070e2f803e60985c7f
Niklas <nvboguszewski@googlemail.com>
2020-10-04 13:50:22 +0200

    Update service.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
@@ -218,7 +218,14 @@ var handle = GroupChatMsg.find().observe({
   added: function (item) {
     if (!initializing)
         //did I make the post request?
-        console.log(item)
+        sender = item.sender
+        console.log(sender)
+        console.log(Auth.userID)
+        if (item.sender == Auth.userID) {
+          console.log('execute command')
+        } else {
+          'dont execute command'
+        }
         make_post_request(item.message)
         console.log('last_intent', last_intent)
   }
