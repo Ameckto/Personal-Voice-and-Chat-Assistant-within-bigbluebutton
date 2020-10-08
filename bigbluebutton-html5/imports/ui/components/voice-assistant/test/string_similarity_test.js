fb425935d91be7a84919b2f657bc2c92f99abe74
Niklas <nvboguszewski@googlemail.com>
2020-10-08 19:38:57 +0200

    Update string_similarity_test.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/test/string_similarity_test.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/test/string_similarity_test.js
@@ -1,6 +1,9 @@
 var string_similarity_test = function string_similarity_test(){
+  var my_test = function() {
+    console.log('in test file')
 
-  console.log('in test file')
 
-}();
+  }();
+
+};
 module.exports.string_similarity_test = string_similarity_test;
