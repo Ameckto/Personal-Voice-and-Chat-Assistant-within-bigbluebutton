634041f22c9803fb16ee347cc4e9de541d45c31f
Niklas <nvboguszewski@googlemail.com>
2020-10-13 13:02:12 +0200

    Update string_similarity_test.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/test/string_similarity_test.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/test/string_similarity_test.js
@@ -3,7 +3,18 @@ var string_similarity_test = function string_similarity_test(){
   window.VoiceAssistent_string_similarity_test = (function () {
     var stringSimilarity = require('string-similarity');
 
-    var input_data_arr = [{'input_name': 'Freddy_93', 'expected_name': 'Freddy', 'names_in_meeting': ['Freddy', 'Klaus', 'Niklas']}];
+    var input_data_arr = [{'input_name': 'Freddy', 'expected_name': 'Freddy_93', 'names_in_meeting': ['Freddy_93', 'Klaus', 'Niklas']},
+              {'input_name': 'niklas', 'expected_name': 'Niklas', 'names_in_meeting': ['Freddy', 'Klaus', 'Niklas']},
+              {'input_name': 'Niklas', 'expected_name': 'niklas', 'names_in_meeting': ['Freddy', 'Klaus', 'niklas']},
+              {'input_name': 'Nikki', 'expected_name': 'Nikki', 'names_in_meeting': ['Nikki', 'Klaus', 'Paulana']},
+              {'input_name': 'Chris', 'expected_name': 'Christian', 'names_in_meeting': ['Christian', 'Klaus', 'Niklas']},
+              {'input_name': 'Christian', 'expected_name': 'Chris', 'names_in_meeting': ['Chris', 'Klaus', 'Niklas']},
+              {'input_name': 'Robert', 'expected_name': 'Rob3ert', 'names_in_meeting': ['Rob3ert', 'Klaus', 'Roberta']},
+              {'input_name': 'Otto', 'expected_name': '0tt0', 'names_in_meeting': ['0tt0', 'Klaus', 'Seid']},
+              {'input_name': 'Robert Johanson', 'expected_name': 'Robert', 'names_in_meeting': ['Robert', 'Klaus', 'Seid']},
+              {'input_name': 'Robert', 'expected_name': 'Robert Johanson', 'names_in_meeting': ['Robert Johanson', 'Klaus', 'Seid']},
+            ];
+            
     var output_data_arr = []
     var evaluateTest = function(test) {
       // util function to sort the result
