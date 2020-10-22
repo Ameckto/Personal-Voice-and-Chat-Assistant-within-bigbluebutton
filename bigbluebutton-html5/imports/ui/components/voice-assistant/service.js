1841bba2a2014847afba9c640dc20093aa54dede
Niklas <nvboguszewski@googlemail.com>
2020-10-22 08:38:21 +0200

    Update service.js

--- bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
+++ bigbluebutton-html5/imports/ui/components/voice-assistant/service.js
@@ -357,9 +357,9 @@ var execute_intent = function(intent, response) {
       break;
 
     case 'summarize':
-      alert('Open Meeting Summary');
       const url = 'https://ltbbb1.informatik.uni-hamburg.de/html5client_niklas/summarize';
       window.open(url, "_blank");
+
       make_notify('summarize', '');
       break;
 
